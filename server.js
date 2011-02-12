768b408da4d00dd792a17e542df1df87045450f6
Marak Squires <marak.squires@gmail.com>
2011-02-12 10:09:43 -0500

    [major fucking refactor] Converted looper into a proper Class. Added Dnode support for RMI. Browser no longer plays audio, it acts as a controller for the sequence. Create simple table view for playing sequence.

---
+++ server.js
@@ -0,0 +1,80 @@
+/*******************************
+JSONloops is a javascript audio sequencer that runs on JSON arrays of sounds
+
+    JSONloops are nested arrays that contain: a nested array, a sound, or null
+    
+    the arrays are nested in the following hierarchy 
+    
+    * SONG
+    *   TRACKS
+    *     MEASURES
+    *       BEATS
+  
+
+  *******************************/
+
+var Looper = require('./lib/looper').Looper,
+fs = require('fs'),
+sys = require('sys');
+
+//var song = fs.readFileSync('./loops/metronome.json');
+
+// Load the NYC.json song
+var song = fs.readFileSync('./loops/nyc.json');
+
+// Load up a drum kit
+var drums = require('./kits/drums').drums;
+
+// Create a new sequencer using the Looper Class
+var sequencer = new Looper(song, drums);
+
+
+
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter;
+
+sequencer.on('step', function(){
+  emitter.emit('step', sequencer);
+});
+
+
+var clients = {};
+function LoopServer (client, con) {
+
+  con.on('ready', function () {
+    // When the client connection is ready, bind the Looper step event to the client's step event
+    emitter.on('step', client.step);
+  });
+
+  con.on('end', function () {
+    // Nothing is bound to the end event for now
+  });
+
+  /* Expose some methods to Dnode, this could probaly be done with a loop of the modules exports */
+  this.create_mix = function ( song ) {
+    song = JSON.parse(song);
+    sequencer.create_mix(song);
+  };
+
+  this.stop = function ( ) {
+    sequencer.stop();
+  };
+
+  this.start = function ( ) {
+    sequencer.start();
+  };
+    
+}
+
+var connect = require('connect');
+var server = connect.createServer()
+    .use(connect.staticProvider(__dirname))
+    .listen(8080);
+
+var DNode = require('dnode');
+DNode(LoopServer).listen(server, {
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(' '),
+});
+
+console.log('http://localhost:8080/');
+
