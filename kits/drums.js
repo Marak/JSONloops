540a81097aa8669f7d35c07876d6a3e921663162
hij1nx <fernmicro@gmail.com>
2011-02-13 13:47:31 -0500

    drums.js

--- kits/drums.js
+++ kits/drums.js
@@ -2,5 +2,12 @@
 exports.drums = {
   kick : './wavs/kick.wav',
   snare : './wavs/snare.wav',
-  tick : './wavs/tick.wav'
+  tick : './wavs/tick.wav',
+  tom : './wavs/tom.wav',
+  bell : './wavs/bell.wav',
+  bongobig : './wavs/bongobig.wav',
+  bongosmall : './wavs/bongosmall.wav',
+  piano : './wavs/piano.wav',
+  ride : './wavs/ride.wav',
+  tambourene : './wavs/tambourene.wav'  
 };
